a3807a40697e48e1e3c70ff5c70e5fd4ef46e677
gwanii <lung4536@gmail.com>
2023-01-03 11:26:06 +0900

    [feat] : 문자열 추가추가추가 테스트 !!!

--- src/main/java/shop/itbook/itbookauth/Test.java
+++ src/main/java/shop/itbook/itbookauth/Test.java
@@ -3,5 +3,8 @@ package shop.itbook.itbookauth;
 public class Test {
     public static void main(String[] args) {
         System.out.println("Hello");
+        System.out.println("adsfad");
+        System.out.println("asdfsafasf");
+
     }
 }
