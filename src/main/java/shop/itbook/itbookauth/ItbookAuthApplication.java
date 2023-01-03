799e71088e88e9eadfb8667cbc1718a7843fa2a6
gwanii <lung4536@gmail.com>
2023-01-03 11:18:10 +0900

    first commit

---
+++ src/main/java/shop/itbook/itbookauth/ItbookAuthApplication.java
@@ -0,0 +1,13 @@
+package shop.itbook.itbookauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItbookAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItbookAuthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/shop/itbook/itbookauth/ItbookAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package shop.itbook.itbookauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItbookAuthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
