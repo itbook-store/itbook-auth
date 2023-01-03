47fdbdd1d9adf623f4c058edde635de50f107800
jun1718 <gbeovhsqhtka@naver.com>
2023-01-03 11:31:16 +0900

    [feat] : 남의것 테스트

--- src/main/java/shop/itbook/itbookauth/ItbookAuthApplication.java
+++ src/main/java/shop/itbook/itbookauth/ItbookAuthApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ItbookAuthApplication {
 
     public static void main(String[] args) {
+        System.out.println("남의거 변경");
         SpringApplication.run(ItbookAuthApplication.class, args);
     }
 
