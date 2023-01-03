807366bb29f21eb2fbbac27f7639232ab08b0535
jun1718 <gbeovhsqhtka@naver.com>
2023-01-03 11:34:49 +0900

    [feat] :  남의거 변경 포크테스트

--- src/main/java/shop/itbook/itbookauth/ItbookAuthApplication.java
+++ src/main/java/shop/itbook/itbookauth/ItbookAuthApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ItbookAuthApplication {
 
     public static void main(String[] args) {
-        System.out.println("남의거 변경");
+        System.out.println("남의거 변경 포크");
         SpringApplication.run(ItbookAuthApplication.class, args);
     }
 
