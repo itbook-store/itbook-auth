2eb5ea22bb20c31de2cb1bb0da86776564da4eed
gwanii <lung4536@gmail.com>
2023-01-04 09:33:35 +0900

    [chore] .gitignore 파일 추가

--- src/main/java/shop/itbook/itbookauth/ItbookAuthApplication.java
+++ src/main/java/shop/itbook/itbookauth/ItbookAuthApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class ItbookAuthApplication {
 
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
     public static void main(String[] args) {
         System.out.println("남의거 변경 포크");
         SpringApplication.run(ItbookAuthApplication.class, args);
